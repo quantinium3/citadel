56af827132022b71b49d75498efb26b97050df04
quantinium3 <quant@quantinium.dev>
2026-04-18 00:11:33 +0530

    chore: added nginx config and enable http3

--- nixos/citadel/modules/networking.nix
+++ nixos/citadel/modules/networking.nix
@@ -3,7 +3,7 @@
     enable = true;
     trustedInterfaces = [ "tailscale0" ];
     allowedTCPPorts = [ 80 443 ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
+    allowedUDPPorts = [ config.services.tailscale.port 443 ];
     extraCommands = ''
       iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
       iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j REJECT --reject-with tcp-reset
--- nixos/citadel/modules/nginx.nix
+++ nixos/citadel/modules/nginx.nix
@@ -1,7 +1,33 @@
+{ ... }:
 {
-	services.nginx = {
-		enable = true;
-	};
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    commonHttpConfig = ''
+      http2 on;
+      ssl_early_data on;
+    '';
+
+    virtualHosts."citadel.quantinium.dev" = {
+      sslCertificate = "/etc/nginx/certificates/citadel.quantinium.dev.pem";
+      sslCertificateKey = "/etc/nginx/certificates/citadel.quantinium.dev.key";
+      forceSSL = true;
+      http3 = true;
+      quic = true;
+
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:5678";
+          extraConfig = ''
+            add_header Alt-Svc 'h3=":443"; ma=86400' always;
+          '';
+        };
+      };
+    };
+  };
+
   systemd.tmpfiles.rules = [
     "d /var/log/nginx 0750 nginx nginx -"
   ];
